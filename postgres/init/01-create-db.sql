038d05ac4a36d1b529e62d85ff945bd34b1abac4
Ivan Gudak <ihudak@gmail.com>
2024-03-31 14:16:13 +0200

    make namespace for databases

--- postgres/init/01-create-db.sql
+++ postgres/init/01-create-db.sql
@@ -1,6 +1,7 @@
 create database wp_categories owner pguser;
 create database wp_users owner pguser;
-create database dt_books owner pguser;
+create database dt_books_books owner pguser;
 create database dt_books_storage owner pguser;
 create database dt_books_orders owner pguser;
+create database dt_books_ingest owner pguser;
 
