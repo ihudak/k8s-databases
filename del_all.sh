5c2597d66150a8ce0b72ef4be9ba3db96613401b
Ivan Gudak <ihudak@gmail.com>
2024-03-31 22:52:00 +0200

    Fix volumes config

--- del_all.sh
+++ del_all.sh
@@ -1,4 +1,8 @@
-for d in */ ; do
-    [ -L "${d%/}" ] && continue
-    kubectl delete -f $d${d%/}.yaml
-done
+#for d in */ ; do
+#    [ -L "${d%/}" ] && continue
+#    kubectl delete -f $d${d%/}.yaml
+#done
+
+kubectl delete -f mongo/mongo.yaml
+kubectl delete -f postgres/postgres.yaml
+kubectl delete -f mysql/mysql.yaml
--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,9 @@
 ./set_cur_dir.sh
 
-for d in */ ; do
-    [ -L "${d%/}" ] && continue
-    kubectl apply -f $d${d%/}.yaml
-done
+#for d in */ ; do
+#    [ -L "${d%/}" ] && continue
+#    kubectl apply -f $d${d%/}.yaml
+#done
+kubectl apply -f mongo/mongo.yaml
+kubectl apply -f postgres/postgres.yaml
+kubectl apply -f mysql/mysql.yaml
